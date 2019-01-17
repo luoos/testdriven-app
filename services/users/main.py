6da36effbb9926d91ee2438063fde561d1b997f8
Jun Luo <luojunmoo@gmail.com>
2019-01-17 17:12:16 +0800

    Setting up Postgres

--- services/users/main.py
+++ services/users/main.py
@@ -1,5 +1,11 @@
 import os
 
-from app import create_app
+from app import create_app, db
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+@app.cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
