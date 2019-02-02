5bdec7d808277ca42cb68c46eac15aa0007c0a45
Jun Luo <luojunmoo@gmail.com>
2019-02-02 18:48:53 +0800

    Fix failing client test cases

--- services/client/src/App.test.js
+++ services/client/src/App.test.js
@@ -2,19 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+import authReducer from './store/reducers/auth';
 import App from './App';
 
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const rootReducer = combineReducers({
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
 
 describe('<App />', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<BrowserRouter><App /></BrowserRouter>);
-  })
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
+    ReactDOM.render(app, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 })
