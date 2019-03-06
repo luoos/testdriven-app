0ab34465f3d2bf865dc2fa07b759ba2c79b7fc70
Jun Luo <luojunmoo@gmail.com>
2019-03-06 14:26:43 -0500

    Set up basic draft editor
    No any style

--- services/client/src/containers/DraftEditor/DraftEditor.js
+++ services/client/src/containers/DraftEditor/DraftEditor.js
@@ -2,12 +2,41 @@ import React from 'react';
 import { Editor, EditorState } from 'draft-js';
 
 class DraftEditor extends React.Component {
+  state = {
+    editorState: EditorState.createEmpty()
+  }
+
+  onChange = (editorState) => {
+    this.setState({editorState});
+  }
+
+  setEditor = (editor) => {
+    this.editor = editor;
+  }
+
+  focusEditor = () => {
+    if (this.editor) {
+      this.editor.focus();
+    }
+  }
+
   render() {
     return (
-      <div>
-        WYSIWYG Editor
+      <div style={styles.editor} onClick={this.focusEditor}>
+        <Editor
+          ref={this.setEditor}
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+        />
       </div>
     );
+  }
+}
+
+const styles = {
+  editor: {
+    border: '1px solid gray',
+    minHeight: '6em'
   }
 }
 
